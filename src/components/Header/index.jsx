d392da70a4eeb8d6739a2bfa022e0c851c53a7a3
krplab <kauan.r.paulino@gmail.com>
2024-10-01 21:01:29 -0300

    seção header de baixa fidelidade implementada

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,11 @@
 import styles from "./headerStyle.module.scss";
+import principal_logo from "../../assets/logos/principal_logo.svg";
 
 export const Header = () => {
     return (
         <header className={styles.headerSection}>
+            <img src={principal_logo} alt="logomarca da empresa bragás aquecedores"></img>
+
             <ul>
                 <li>Serviços</li> |
                 <li>Quem Somos</li> |
